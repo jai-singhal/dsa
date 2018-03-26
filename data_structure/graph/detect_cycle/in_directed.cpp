aa78c771e44e3c8df50115d29fedc508ed1f1aa3
Jai Singhal <jaisinghal48@gmail.com>
2018-03-26 20:43:57 +0530

    directed detect cycle done

--- data_structure/graph/detect_cycle/in_directed.cpp
+++ data_structure/graph/detect_cycle/in_directed.cpp
@@ -5,32 +5,35 @@ class GraphDetect: public DirectedGraph{
     public:
         GraphDetect(int v): DirectedGraph(v){}
 
-        bool isCycleUtil(int s, bool* visited){
+        bool isCycleUtil(int s, bool* visited, bool* recursionStack){
             
             if( !visited[s] ){
                 visited[s] = true;
+                recursionStack[s] = true;
                 list<int>:: iterator it;
-                cout << s << " x " << visited[s] << endl;                
                 for(it = this->adj[s].begin(); it != this->adj[s].end(); it++){
-                    if( !visited[*it] && isCycleUtil(*it, visited))
+                    if( !visited[*it] && isCycleUtil(*it, visited, recursionStack))
                         return true;
-                    else    
-                        return false;
+                    else if (recursionStack[*it]) // if the value found in recursion stack
+                        return true;       
                 }
             }
+            recursionStack[s] = false;
+            return false;
         }
 
         bool isCycle(){
             bool* visited = new bool[this->V];
+            bool* recursionStack = new bool[this->V];
             int i, j;
-            for(j = 0;  j < this->V; j++)
+            for(j = 0;  j < this->V; j++){
                 visited[j] = false;
-            for(i = 0; i < this->V; i++){
-
+                recursionStack[j] = false;
+            }
                 
-                if (isCycleUtil(i, visited))
+            for(i = 0; i < this->V; i++){
+                if (isCycleUtil(i, visited, recursionStack))
                     return true;  
-                cout << endl;
             }
             return false;
         }
@@ -38,12 +41,14 @@ class GraphDetect: public DirectedGraph{
 
 
 int main(void){
-    GraphDetect g(2);
+    GraphDetect g(4);
     g.addEdge(0, 1);
-    g.addEdge(0, 0);
-    // g.addEdge(2, 3);
-    g.printGraph();
-    cout << g.isCycle() << "ans";
+    g.addEdge(0, 2);
+    g.addEdge(1, 2);
+    g.addEdge(2, 0);
+    g.addEdge(2, 3);
+    g.addEdge(3, 3);
+    cout << g.isCycle();
     return 0;
 }
 
